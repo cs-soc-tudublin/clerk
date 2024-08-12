fa022e4448366eafa6ba0b56ffe9572adf53f4c6
David Lynch <davite3@protonmail.com>
2024-08-12 02:10:52 +0100

    Ran prettier on the codebase, ignored yaml and md files Used the command "npx prettier --write ." on the repo.
    The actual semantics of the code itself hasn't been changed. Its just
    formatted to look nicer.
    
    Prettier will parse the editorconfig and format the project according to
    it.

--- src/docsModal.ts
+++ src/docsModal.ts
@@ -1,47 +1,50 @@
 import { App, Modal, Setting } from "obsidian";
 
 export class docsModal extends Modal {
-    title: string;
-    tags: string;
-    onSubmit: (result: { title: string, tags: string }) => void;
+	title: string;
+	tags: string;
+	onSubmit: (result: { title: string; tags: string }) => void;
 
-    constructor(app: App, onSubmit: (result: { title: string, tags: string }) => void) {
-        super(app);
-        this.onSubmit = onSubmit;
-    }
+	constructor(
+		app: App,
+		onSubmit: (result: { title: string; tags: string }) => void,
+	) {
+		super(app);
+		this.onSubmit = onSubmit;
+	}
 
-    onOpen() {
-        const { contentEl } = this;
+	onOpen() {
+		const { contentEl } = this;
 
-        contentEl.createEl("h1", { text: "Create a New Help Doc" });
+		contentEl.createEl("h1", { text: "Create a New Help Doc" });
 
-        new Setting(contentEl)
-            .setName("Post Title")
-            .addText((text) =>
-                text.onChange((value) => {
-                    this.title = value
-        }));
+		new Setting(contentEl).setName("Post Title").addText((text) =>
+			text.onChange((value) => {
+				this.title = value;
+			}),
+		);
 
-        new Setting(contentEl)
-                .setName("Post Tags (Separated by spaces)")
-                .addText((text) =>
-                text.onChange((value) => {
-                        this.tags = value
-        }));
+		new Setting(contentEl)
+			.setName("Post Tags (Separated by spaces)")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.tags = value;
+				}),
+			);
 
-        new Setting(contentEl)
-            .addButton((btn) =>
-                btn
-                    .setButtonText("Submit")
-                    .setCta()
-                    .onClick(() => {
-                        this.close();
-                        this.onSubmit({ title: this.title, tags: this.tags });
-                    }));
-    }
+		new Setting(contentEl).addButton((btn) =>
+			btn
+				.setButtonText("Submit")
+				.setCta()
+				.onClick(() => {
+					this.close();
+					this.onSubmit({ title: this.title, tags: this.tags });
+				}),
+		);
+	}
 
-    onClose() {
-        const { contentEl } = this;
-        contentEl.empty();
-    }
+	onClose() {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
 }
